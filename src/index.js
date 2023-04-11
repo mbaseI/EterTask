8e2556777aafed21b853c426f86502fe05fca57d
Mustafa Basel <mustafa.basel@outlook.com>
2023-04-12 02:01:30 +0300

    Bug Fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
